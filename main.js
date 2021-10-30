d4e72ded5a0012412f8b57c065164fa0ef90727a
Bassel <bassel444555@gmail.com>
2021-10-30 18:50:07 +0200

    add js

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 let section = document.querySelector('.skills');
 let span = document.querySelectorAll('.skill span');
 let sections = document.querySelectorAll('.s');
-window.location.href = '#events'
-window.onscroll = function() {
+// Go to top btn
+let btn = document.createElement('i');
+btn.classList = 'fa fa-arrow-up to-top'
+document.body.appendChild(btn);
+
+// Calc Scrolling
+let progress = document.querySelector('.progress');
+let scroll = document.querySelector('footer').offsetTop;
+
+
+window.addEventListener('scroll', function() {
+  // Auto Fill progress
   if (scrollY >= section.offsetTop - 200) {
     span.forEach((span) => {
       span.style.width = span.dataset.width;
     });
   }
+
+  // Animation show Section
   sections.forEach((section) => {
     if (scrollY >= section.offsetTop - 500) {
       section.style.animation = `
       show-section 1.6s both linear`;
     }
   });
-}
+  // Scroll To to Btn
+  if (scrollY >= 1000) {
+    btn.classList.add('show');
+  } else {
+    btn.classList.remove('show');
+  }
+  let calc = scrollY * 100 / scroll;
+  progress.style.width = `${calc}%`;
+
+});
 
 // Start Count Down
 
@@ -43,3 +64,44 @@ let counter = setInterval(() => {
     clearInterval(counter);
   }
 }, 1000);
+
+// Go to top btn
+btn.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  })
+});
+let inputs = document.querySelectorAll('input:not([type=submit])');
+let submits = document.querySelectorAll('[type=submit]');
+submits.forEach((submit) => {
+  submit.addEventListener('click', (e) => {
+    inputs.forEach(input => {
+      if (input.value == '') {
+        e.preventDefault();
+        console.log(true)
+      }
+    });
+  });
+});
+
+// View img
+let imgs = document.querySelectorAll('img');
+let v_img = document.createElement('img');
+let view = document.createElement('div');
+view.appendChild(v_img);
+view.classList.add('view-img');
+
+// img viewers
+imgs.forEach((img) => {
+  img.addEventListener('click', () => {
+    // get src with img and add to view
+    v_img.src = img.src;
+    view.classList.add('v');
+    document.body.appendChild(view);
+  });
+});
+// remov view
+view.addEventListener('click', () => {
+  view.classList.remove('v')
+});
